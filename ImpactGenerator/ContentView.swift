f476e4121f420d7723ab52d95228204eca19e979
yabby1997 <yabby1997@gmail.com>
2024-07-20 18:45:53 +0900

    Initial Commit

---
+++ ImpactGenerator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImpactGenerator
+//
+//  Created by Seunghun on 7/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImpactGenerator/ImpactGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImpactGeneratorApp.swift
+//  ImpactGenerator
+//
+//  Created by Seunghun on 7/20/24.
+//
+
+import SwiftUI
+
+@main
+struct ImpactGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
